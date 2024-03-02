282c52e969ecfef22399a964695299e08e319c0a
GuilhermeHenriqueC <coliados.gui@gmail.com>
2024-03-02 18:42:58 +0000

    Create calculator.c

---
+++ calculator.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+
+void mutliply(int);
+void subtract(int);
+void addition(int);
+void division(int);
+void oper(char);
+
+int main(){
+    int num1 = 0;
+    int num2 = 0;
+    char maths;
+    int add, minus, divide, mult;
+ 
+    
+
+printf("First number:  ");
+scanf("%d", &num1);
+
+printf("\nmathematical operator(+ , - , x , /) : ");
+scanf("%c", &maths);
+
+printf("%c", maths);
+
+
+//oper(maths);
+
+    
+
+    
+
+    return 0;
+}
+
+void oper(char math){
+
+int add, minus, divide, mult  = 0;
+
+
+    switch (math)
+{
+case '+':
+    add = 1;
+    break;
+
+case '-':
+    minus = 1;
+    
+    break;
+case 'x':
+    mult = 1;
+    
+    break;
+case '/':
+    divide = 1;
+    
+    break;
+
+
+printf("%d", minus);
+}
+}
